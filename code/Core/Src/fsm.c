8f238103c05791952e8749b22ea6b2f2109bf866
DESKTOP-KB3ML6G\Admin <chuongdaden3011@gamil.com>
2025-10-26 14:21:29 +0700

    đã có đèn giao thông

--- code/Core/Src/fsm.c
+++ code/Core/Src/fsm.c
@@ -1,12 +1,13 @@
 #include "fsm.h"
 #include "software_timer.h"
+#include "main.h"
 
 int led_buffer[4] = {1, 2, 3, 4};
 int led1_time = 0;
 int led2_time = 0;
-int r_time = 5;
-int g_time = 3;
-int y_time= 0;
+int r_time = 15;
+int g_time = 12;
+int y_time= 3;
 int idx =0;
 
 void display7SEG(int num) {
@@ -29,15 +30,91 @@ void update7SEG(int index){
 	HAL_GPIO_WritePin(GPIOA, EN0_Pin << index, GPIO_PIN_RESET);
 }
 
+void setTrafficLED(int r1, int g1, int y1, int r2, int g2, int y2) {
+    HAL_GPIO_WritePin(GPIOA, RED_1_Pin, (r1 ? GPIO_PIN_SET : GPIO_PIN_RESET));
+    HAL_GPIO_WritePin(GPIOA, GREEN_1_Pin, (g1 ? GPIO_PIN_SET : GPIO_PIN_RESET));
+    HAL_GPIO_WritePin(GPIOA, YELLOW_1_Pin, (y1 ? GPIO_PIN_SET : GPIO_PIN_RESET));
+
+    HAL_GPIO_WritePin(GPIOA, RED_2_Pin, (r2 ? GPIO_PIN_SET : GPIO_PIN_RESET));
+    HAL_GPIO_WritePin(GPIOA, GREEN_2_Pin, (g2 ? GPIO_PIN_SET : GPIO_PIN_RESET));
+    HAL_GPIO_WritePin(GPIOA, YELLOW_2_Pin, (y2 ? GPIO_PIN_SET : GPIO_PIN_RESET));
+}
+
+void enterState(int new_state, int t1, int t2,
+                int r1, int g1, int y1, int r2, int g2, int y2) {
+    status = new_state;
+    led1_time = t1;
+    led2_time = t2;
+    setTrafficLED(r1, g1, y1, r2, g2, y2);
+    setTimer1(1000); // 1 giây
+}
 
 
-void fsm_auto_2way_run(){
+void Scan_7seg(){
 	if(timer2_flag == 1){
-		setTimer2(250); // Đặt lại timer cho chu kỳ quét tiếp theo
+		setTimer2(50);
 		update7SEG(idx);
 		idx = (idx + 1) % 4;
 	}
 	else if(timer2_cnt ==0){
-		setTimer2(250);
+		setTimer2(50);
 	}
+}
+
+void fsm_auto_2way_run(){
+	if (timer1_flag) {
+			// Giảm thời gian mỗi giây
+			if (led1_time > 0) led1_time--;
+			if (led2_time > 0) led2_time--;
+
+			setTimer1(1000); // lặp lại 1 giây
+		}
+
+		switch (status) {
+		case INIT:
+			enterState(AUTO_R1_G2, r_time, g_time,
+					   GPIO_PIN_RESET, GPIO_PIN_SET, GPIO_PIN_SET,
+					   GPIO_PIN_SET, GPIO_PIN_RESET, GPIO_PIN_SET);
+			break;
+
+		case AUTO_R1_G2:
+			if (led2_time <= 0) {
+				enterState(AUTO_R1_Y2, led1_time, y_time,
+						   GPIO_PIN_RESET, GPIO_PIN_SET, GPIO_PIN_SET,
+						   GPIO_PIN_SET, GPIO_PIN_SET, GPIO_PIN_RESET);
+			}
+			break;
+
+		case AUTO_R1_Y2:
+			if (led2_time <= 0) {
+				enterState(AUTO_G1_R2, g_time, r_time,
+						   GPIO_PIN_SET, GPIO_PIN_RESET, GPIO_PIN_SET,
+						   GPIO_PIN_RESET, GPIO_PIN_SET, GPIO_PIN_SET);
+			}
+			break;
+
+		case AUTO_G1_R2:
+			if (led1_time <= 0) {
+				enterState(AUTO_Y1_R2, y_time, led2_time,
+						   GPIO_PIN_SET, GPIO_PIN_SET, GPIO_PIN_RESET,
+						   GPIO_PIN_RESET, GPIO_PIN_SET, GPIO_PIN_SET);
+			}
+			break;
+
+		case AUTO_Y1_R2:
+			if (led1_time <= 0) {
+				enterState(AUTO_R1_G2, r_time, g_time,
+						   GPIO_PIN_RESET, GPIO_PIN_SET, GPIO_PIN_SET,
+						   GPIO_PIN_SET, GPIO_PIN_RESET, GPIO_PIN_SET);
+			}
+			break;
+
+		default:
+			// fallback an toàn: đưa về INIT nếu giá trị không hợp lệ
+			enterState(INIT, 0, 0,
+					   GPIO_PIN_SET, GPIO_PIN_SET, GPIO_PIN_SET,
+					   GPIO_PIN_SET, GPIO_PIN_SET, GPIO_PIN_SET);
+			break;
+		}
+	Scan_7seg();
 }
--- code/Debug/Core/Src/fsm.d
+++ code/Debug/Core/Src/fsm.d
@@ -24,7 +24,7 @@ Core/Src/fsm.o: ../Core/Src/fsm.c ../Core/Inc/fsm.h ../Core/Inc/global.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- ../Core/Inc/button.h ../Core/Inc/software_timer.h
+ ../Core/Inc/button.h ../Core/Inc/software_timer.h ../Core/Inc/main.h
 
 ../Core/Inc/fsm.h:
 
@@ -85,3 +85,5 @@ Core/Src/fsm.o: ../Core/Src/fsm.c ../Core/Inc/fsm.h ../Core/Inc/global.h \
 ../Core/Inc/button.h:
 
 ../Core/Inc/software_timer.h:
+
+../Core/Inc/main.h:
